04384c1fd1055e5be1a06e6d707d156165a071f0
Gerben Biesbroek <gerbenbiesbroek@live.nl>
2023-07-25 02:13:03 +0200

    filescoped namespace

--- csharpcore/GildedRose/GildedRose.cs
+++ csharpcore/GildedRose/GildedRose.cs
@@ -1,52 +1,51 @@
-﻿namespace GildedRoseKata
+﻿namespace GildedRoseKata;
+
+public class GildedRose
 {
-    public class GildedRose
+    private const int _minQuality = 0;
+    private const int _maxQuality = 50;
+    private const int _backstage_threshold_10 = 10;
+    private const int _backstage_threshold_5 = 5;
+
+    public List<Item> Items { get; private init; }
+
+    public GildedRose(List<Item> Items)
     {
-        private const int _minQuality = 0;
-        private const int _maxQuality = 50;
-        private const int _backstage_threshold_10 = 10;
-        private const int _backstage_threshold_5 = 5;
+        this.Items = Items;
+    }                   
 
-        public List<Item> Items { get; private init; }
-
-        public GildedRose(List<Item> Items)
+    public void UpdateQuality()
+    {
+        foreach (Item item in Items)
         {
-            this.Items = Items;
-        }                   
-
-        public void UpdateQuality()
-        {
-            foreach (Item item in Items)
+            int qualityChange = DetermineQualityChange(item);
+            if (item.Name != ItemNames.Sulfuras)
             {
-                int qualityChange = DetermineQualityChange(item);
-                if (item.Name != ItemNames.Sulfuras)
-                {
-                    --item.SellIn;
-                    item.Quality = Math.Clamp(item.Quality + qualityChange, _minQuality, _maxQuality);
-                }                
-            }
+                --item.SellIn;
+                item.Quality = Math.Clamp(item.Quality + qualityChange, _minQuality, _maxQuality);
+            }                
         }
-
-        private static int DetermineQualityChange(Item item)
-        {
-            bool beforeSellDate = item.SellIn > 0;
-            return item.Name switch
-            {
-                ItemNames.Sulfuras      => 0,
-                ItemNames.ConjuredMana  => beforeSellDate ? -2 : -4,
-                ItemNames.AgedBrie      => beforeSellDate ? 1 : 2,
-                ItemNames.BackstagePass => BackStageQualityChange(item),
-                _                       => beforeSellDate ? -1 : -2
-            };
-        }
-
-        private static int BackStageQualityChange(Item item) =>
-            item.SellIn switch
-            {
-                <= 0                        => -item.Quality,
-                <= _backstage_threshold_5   => 3,
-                <= _backstage_threshold_10  => 2,
-                _                           => 1
-            };
     }
+
+    private static int DetermineQualityChange(Item item)
+    {
+        bool beforeSellDate = item.SellIn > 0;
+        return item.Name switch
+        {
+            ItemNames.Sulfuras      => 0,
+            ItemNames.ConjuredMana  => beforeSellDate ? -2 : -4,
+            ItemNames.AgedBrie      => beforeSellDate ? 1 : 2,
+            ItemNames.BackstagePass => BackStageQualityChange(item),
+            _                       => beforeSellDate ? -1 : -2
+        };
+    }
+
+    private static int BackStageQualityChange(Item item) =>
+        item.SellIn switch
+        {
+            <= 0                        => -item.Quality,
+            <= _backstage_threshold_5   => 3,
+            <= _backstage_threshold_10  => 2,
+            _                           => 1
+        };
 }
